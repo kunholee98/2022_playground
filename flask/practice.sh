dc795868cb11e56b7721d920874cd9517f4483f4
kunholee_98 <dlrjsgh750@naver.com>
2022-01-18 17:43:03 +0900

    flask 세팅

---
+++ flask/practice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Users/kunolee_98/practice_programming/2022_playground/flask/projects/practice
+export FLASK_APP=pybo
+export FLASK_ENV=development
+source /Users/kunolee_98/practice_programming/2022_playground/flask/venvs/practice/bin/activate
