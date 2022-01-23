f60fdc36bf8a5558111a3370f3b41143d20e4111
kunholee_98 <dlrjsgh750@naver.com>
2022-01-23 16:32:00 +0900

    질문, 답글 수정 & 삭제

--- flask/projects/practice/pybo/views/answer_views.py
+++ flask/projects/practice/pybo/views/answer_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request, render_template, g
+from flask import Blueprint, url_for, request, render_template, g,flash
 from werkzeug.utils import redirect
 
 from .. import db
@@ -30,3 +30,35 @@ def create(question_id):
     # question.answer_set.append(answer)
     # db.session.commit()
     # return redirect(url_for('question.detail', question_id=question.id))
+
+@bp.route('/modify/<int:answer_id>', methods=('GET', 'POST'))
+@login_required
+def modify(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash("수정 권한이 없습니다.")
+        return redirect(url_for('question.detail', question_id=answer.questionId))
+    if request.method == 'POST':
+        form = AnswerForm()
+        if form.validate_on_submit():
+            # form에 적힌 정보를 answer에 복사
+            form.populate_obj(answer)
+            answer.updatedAt = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=answer.questionId))
+    else:
+        form = AnswerForm(obj=answer)
+    return render_template('answer/answer_form.html', form=form)
+
+
+@bp.route('/delete/<int:answer_id>')
+@login_required
+def delete(answer_id):
+    answer = Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash("삭제권한이 없습니다.")
+        return redirect(url_for('question.detail', question_id=answer.questionId))
+    db.session.delete(answer)
+    db.session.commit()
+    return redirect(url_for('question.detail', question_id=answer.questionId))
+    
