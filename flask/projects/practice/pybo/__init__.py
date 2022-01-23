4c167037b5876f094034055c0c5832629fca9c8f
kunholee_98 <dlrjsgh750@naver.com>
2022-01-23 14:52:33 +0900

    SQLite MetaData 설정

--- flask/projects/practice/pybo/__init__.py
+++ flask/projects/practice/pybo/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 import config
 
+naming_convention = {   # SQLite db 에서는 사용하는 인덱스 등의 제약 조건 이름을 MetaData 클래스를 사용하여 규칙 정의가 필요
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 # create_app 밖에서 선언하여 다른 모듈에서도 사용할 수 있다.
 # 객체 초기화만 create_app 내에서 진행
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
@@ -18,7 +27,10 @@ def create_app():
 
     # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):  # sqlite db를 사용할 때 오류 방지
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from . import models
 
     # @ 애너테이션으로 매핑되는 함수는 라우트 함수라 부른다.
