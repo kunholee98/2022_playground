88303b74a643fd38e894996e669bbda4fa4a72a9
kunholee_98 <dlrjsgh750@naver.com>
2022-03-24 23:46:18 +0900

    후위표기법

---
+++ baekjoon/2203/posterior_notation.py
@@ -0,0 +1,28 @@
+expression = input()
+
+stack = []
+ans = ""
+for s in expression:
+    if s == '+' or s == '-':
+        while stack and stack[-1] != '(':
+            ans += stack.pop()
+        stack.append(s)
+    elif s == '*' or s == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
+            ans += stack.pop()
+        stack.append(s)
+    elif s == '(':
+        stack.append(s)
+    elif s == ')':
+        while stack and stack[-1] != '(':
+            ans += stack.pop()
+        stack.pop()
+    else:
+        ans += s
+
+while stack:
+    ans += stack.pop()
+
+print(ans)
+
+# 이해합시다!
